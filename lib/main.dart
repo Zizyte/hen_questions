5e20db1cdc69576988828190152feb459d920055
Zizyte <martynabertule@gmail.com>
2026-03-22 13:39:58 +0200

    netlify test version

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-const String kAppVersion = "HELLO NEW VERSION";
+const String kDebugVersion = "NETLIFY TEST 777";
 
 void main() => runApp(const MyApp());
 
